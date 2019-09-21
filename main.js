067b1bdf723587f052579b2f27990356a6cd460e
Gautam Samudrala <gautam.samudrala@gmail.com>
2019-09-21 16:33:44 -0700

    Updates js files

--- main.js
+++ main.js
@@ -8,4 +8,17 @@ class main{
     function adddoctor(var d){
         doctors.push(d);
     }
+    function userLogin(var name, var password){
+        current = null;
+        for(var i = 0; i < users.length; i++){
+            if(users[i].getPassword.equals(password) && user[i].getUserName.equals(name));{
+                current = i;
+                return true;
+            }
+        }
+        if(current == null){
+            alert("Invalid username or password");
+            return false;
+        }
+    }
 }
--- user.js
+++ user.js
@@ -1,35 +1,34 @@
 class user{
-    var email;
-    var firstName;
-    var lastName;
-    var userName;
-    var weight;
-    var age;
-    var height;
-    var gender;
-    constructor(){
+this.email = null;
+this.firstName = null;
+this.lastName = null;
+this.userName = null;
+this.weight = null;
+this.age = null;
+this.height = null;
+this.gender = null;
+this.password = null;
+constructor(){
         
-    }
-    function setFirstName(var e){
-        firstName = e;
-    }
-    function setLastName(var e){
-        lastName = e;
-    }
-    function setUserName(var e){
-        userName = e;
-    }
-    function setEmail(var e){
-        email = e;
-    }
-    function setAge(var e){
-        age = e;
-    }
-    function setGender(var g){
-        gender = g;
-    }
-    function setHeight(var feet, var inches){
-        height = feet + (inches / 12);
-    }
-    
+}
+this.setEmail = function(email) { this.email = email; } 
+this.getEmail = function() { return this.email; } 
+this.setFirstName = function(firstName) { this.firstName = firstName; } 
+this.getFirstName = function() { return this.firstName; } 
+this.setLastName = function(lastName) { this.lastName = lastName; } 
+this.getLastName = function() { return this.lastName; } 
+this.setUserName = function(userName) { this.userName = userName; } 
+this.getUserName = function() { return this.userName; } 
+this.setWeight = function(weight) { this.weight = weight; } 
+this.getWeight = function() { return this.weight; } 
+this.setAge = function(age) { this.age = age; } 
+this.getAge = function() { return this.age; } 
+this.setHeight = function(height) { this.height = height; } 
+this.getHeight = function() { return this.height; } 
+this.setGender = function(gender) { this.gender = gender; } 
+this.getGender = function() { return this.gender; } 
+this.setPassword = function(password) { this.password = password; } 
+this.getPassword = function() { return this.password; } 
+
+   
 }
