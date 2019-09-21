f346172e9ad7daba7f9488f7cd08dc0e21412c57
Gautam Samudrala <gautam.samudrala@gmail.com>
2019-09-21 16:10:54 -0700

    Update doctor.js

---
+++ doctor.js
@@ -0,0 +1,35 @@
+class doctor{
+    var email;
+    var dataBirth;
+    var firstName;
+    var lastName;
+    var userName;
+    var weight;
+    var age;
+    var medicalLiscenseID;
+    constructor(){
+        
+    }
+    function setFirstName(var e){
+        firstName = e;
+    }
+    function setLastName(var e){
+        lastName = e;
+    }
+    function setUserName(var e){
+        userName = e;
+    }
+    function setEmail(var e){
+        email = e;
+    }
+    function setAge(var e){
+        age = e;
+    }
+    function setmedicalLiscenseID(var e){
+        medicalLiscenseID = e;
+    }
+    function setdataBirth(var b){
+        dateBirth = b;
+    }
+
+}
