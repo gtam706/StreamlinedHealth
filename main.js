60b6e92148315f709b1abb75e2d6d96bca59b1f3
Gautam Samudrala <gautam.samudrala@gmail.com>
2019-09-21 19:35:30 -0700

    Update main.js

--- main.js
+++ main.js
@@ -40,8 +40,21 @@ function formdata()
 {
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
-var doctor = document.getElementById("doctor").value;
-var  
+var choice = getRadioVal(document.getElementById("Radio"),document.getElementsByTagName('input'));
 var bool = doctorLogin(username, password);
 }   
+function getRadioVal(form, name) {
+    var val;
+    // get list of radio buttons with specified name
+    var radios = form.elements[name];
+    
+    // loop through list of radio buttons
+    for (var i=0, len=radios.length; i<len; i++) {
+        if ( radios[i].checked ) { // radio checked?
+            val = radios[i].value; // if so, hold its value in val
+            break; // and break out of for loop
+        }
+    }
+    return val; // return value of checked radio or undefined if none checked
+}
 }
