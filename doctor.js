da4df5d1bdf1169fbae1b28fcbb310f321bebd10
Gautam Samudrala <gautam.samudrala@gmail.com>
2019-09-21 20:38:39 -0700

    Update doctor.js

--- doctor.js
+++ doctor.js
@@ -12,13 +12,7 @@ this.state = null;
 this.country = null;  
 this.userName = null;
 this.password = null;
-function formdata() 
-{
-var firstname1= document.getElementById("firstname").value;
-var lastname1= document.getElementById("lastname").value;
-document.writeln("<h1>Confirmation Page</h1><br>");
-document.writeln("Thank you for completing this form.<br><br>");
-document.writeln("The first name you entered is " + firstname + "<br>");
-document.writeln("The last name you entered is " + lastname);
-}
+    constructor(){
+        
+    }
 }
