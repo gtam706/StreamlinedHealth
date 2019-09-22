2125a2aed4920979fe5537301d596f871c5f70fa
Gautam Samudrala <gautam.samudrala@gmail.com>
2019-09-21 23:06:56 -0700

    Test data

--- doctor.js
+++ doctor.js
@@ -1,5 +1,5 @@
 class doctor{
-this.firstname = null;
+this.firstName = null;
 this.lastName = null;  
 this.email = null;
 this.gender = null; 
@@ -15,4 +15,20 @@ this.password = null;
     constructor(){
         
     }
+constructor(var f, var l, var e, var g, var d, var i, var b, var s, var c, var st,var co, var u, var p){
+    this.firstName = f;
+this.lastName = l;  
+this.email = e;
+this.gender = g; 
+this.dob = d;
+this.ID  = i;
+this.buildingNumber = b;
+this.streetName = s; 
+this.city = c;
+this.state = st; 
+this.country = co;  
+this.userName = u;
+this.password = p;
+}
+
 }
